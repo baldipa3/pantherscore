55a53c258a902e5563fa13e21fdf75b630dd8427
Hernan Romero <hernrome@gmail.com>
2020-02-25 21:13:28 +0000

    Configurated Devise

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,9 +1,10 @@
 class Service < ApplicationRecord
   has_many :reviews
   has_many :users, through: :user_services
-  has_many :categories, presence: true
+  has_many :categories
   validates :name, presence: true, uniqueness: true
   validates :url, presence: true
   validates :description, presence: true, uniqueness: true
+  validates :categories, presence: true
   # has_one_attached :photo
 end
