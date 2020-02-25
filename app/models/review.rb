b3fb21db864b2e0924da19de28bca2bab0e9cc4c
Pablo Baldini <pablo.baldini@gmail.com>
2020-02-25 20:22:19 +0000

    Rating validation added

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :service
 
-  validates :rating, presence: true
+  validates :rating, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: 10, only_integer: true }
   validates :content, presence: true, length: { minimum: 280 }
 end
