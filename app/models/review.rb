3c6aa41f09c1d0c2a535f6272e31c6502d5aa282
Pablo Baldini <pablo.baldini@gmail.com>
2020-02-25 19:54:41 +0000

    Review model migration added

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :user
-
-  validates :rating, presence: true
-  validates :content, presence: true, length: { minimum: 280 }
+  belongs_to :service
 end
--- db/migrate/20200225195404_create_reviews.rb
+++ db/migrate/20200225195404_create_reviews.rb
@@ -4,6 +4,7 @@ class CreateReviews < ActiveRecord::Migration[5.2]
       t.text :content
       t.integer :rating
       t.references :user, foreign_key: true
+      t.references :service, foreign_key: true
 
       t.timestamps
     end
