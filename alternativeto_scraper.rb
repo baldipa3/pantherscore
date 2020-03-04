35fd2f674ceb61059f6088658df2435b029dec56
Hernan Romero <hernrome@gmail.com>
2020-03-04 15:46:06 +0000

    Added exceptions and json export to scraper

--- alternativeto_scraper.rb
+++ alternativeto_scraper.rb
@@ -1,8 +1,9 @@
 require 'rails'
 require 'mechanize'
 require 'pry'
-require 'down'
-require 'selenium-webdriver'
+require 'json'
+# require 'down'
+# require 'selenium-webdriver'
 
 def trigger_scrape
   agent = Mechanize.new
@@ -16,64 +17,92 @@ end
 
 def category_pagination(category_link)
   page = 1
-  2.times do
+  25.times do
     category_page = category_link + "?p=#{page}"
     category_loop(category_page)
     page += 1
   end
 end
 
-def service_scrape(service, description_element)
+def service_scrape(service, resolved_link, slug, more_alternatives_link, description_element)
   # name
-  p name = service.at_css('h1').text.strip
+  name = service.at_css('h1').text.strip
+  alternativeto_url = resolved_link
   # discontinued?
-  discontinued = service.at_css('.label-discontinued').nil?
-  p discontinued ? true : false
+  discontinued = service.at_css('.label-discontinued')
+  discontinued = discontinued.present?
   # lead
-  p lead = service.at_css('.lead').children[0].text.strip
+  lead = service.at_css('.lead').children[0].text.strip
   # company
-  company = service.at_css('.lead').children[1]
-  p company.text.strip if company.present?
-  # company_url
-  company_url = service.at_css('.lead').children[1]
-  p company_url['href'] if company.present?
+  company = service.at_css('.creator > a')
+  company_name = company.text.strip.gsub('Created by ', '') if company.present?
+  company_url = company['href'] if company.present?
   # description
-  p description = service.at_css(description_element).text
+  description = service.at_css(description_element).text
   # url
   url = service.at_css('.icon-official-website')
-  p url['href'] if url.present?
+  url = url['href'] if url.present?
   # categories
-  categories = service.css('.label[href^="/category"]').each { |category| p category.text.strip }
+  categories_array = []
+  categories = service.css('.label[href^="/category"]').each { |category| categories_array << category.text.strip }
   # logo
   icon = service.at_css('img[src^="//d2.alternativeto.net/dist/icons"]')
-  p icon_link = "https:#{icon['src']}".gsub(/=\d+/, '=200')
-  Down.download(icon_link, destination: "../../../Downloads/")
+  icon_url = "https:#{icon['src']}".gsub(/=\d+/, '=200')
+  agent = Mechanize.new
+  agent.user_agent = 'Custom agent'
+  # agent.get(icon_url).body_io.string
+  # Base64.encode64(agent.get(icon_url).body_io.string)
+  encoded_icon = Base64.encode64(agent.get(icon_url).body_io.read)
+  File.open("../../../Downloads/#{slug}.png", "wb") {|f| f.write(Base64.decode64(encoded_icon))}
+  # alternatives
+  alternatives_array = []
+  alternatives = service.css('h3 a[href^="/software"]').each do |alternative|
+    alternatives_array << {name: "#{alternative.text.strip}", slug: "#{alternative['href'].match(/software\/(.*?)\//)[1]}"}
+  end
+  # more alternatives
+  if more_alternatives_link.present?
+    alternatives = more_alternatives_link.click
+    more_alternatives = alternatives.css('h3 a[href^="/software"]').each do |alternative|
+      alternatives_array << {name: "#{alternative.text.strip}", slug: "#{alternative['href'].match(/software\/(.*?)\//)[1]}"}
+    end
+  end
+  puts JSON.pretty_generate({
+    name: name,
+    alternativeto_url: alternativeto_url,
+    slug: slug,
+    discontinued: discontinued,
+    lead: lead,
+    company_name: company_name,
+    company_url: company_url,
+    description: description,
+    url: url,
+    categories: categories_array,
+    alternatives: alternatives_array,
+    icon_url: icon_url,
+    encoded_icon: encoded_icon
+  })
 end
 
 def service_loop(services_links)
   services = services_links.map do |link|
-    service = link.click
+    # service_link
+    resolved_link = link.resolved_uri.to_s
+    slug = resolved_link.match(/software\/(.*?)\//)[1]
+
+    begin
+      service = link.click
+    rescue Mechanize::ResponseCodeError => e
+      next if e.response_code == '404'
+    end
+
     about_link = service.link_with(class: 'btn btn-complementary-view')
     more_alternatives_link = service.link_with(text: " NEXT PAGE ")
-    # alternatives
-    alternatives = service.css('h3 a[href^="/software"]').each do |alternative|
-      p alternative.text.strip
-      p alternative_url = "https://alternativeto.net#{alternative['href']}"
-    end
-    # more alternatives
-    if more_alternatives_link.present?
-      alternatives = more_alternatives_link.click
-      more_alternatives = alternatives.css('h3 a[href^="/software"]').each do |alternative|
-        p alternative.text.strip
-        p alternative_url = "https://alternativeto.net#{alternative['href']}"
-      end
-    end
 
     if about_link.present?
       service = about_link.click
-      service_scrape(service, '.main-info p')
+      service_scrape(service, resolved_link, slug, more_alternatives_link, '.main-info p')
     else
-      service_scrape(service, '.item-desc')
+      service_scrape(service, resolved_link, slug, more_alternatives_link, '.item-desc')
     end
   end
 end
