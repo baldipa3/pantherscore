1923aa8e1944ac124dbc68fa1a6a12ba1d734579
Pablo Baldini <pablo.baldini@gmail.com>
2020-03-26 07:07:13 +0000

    Some alternatives removed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -109,7 +109,7 @@ const request = (url) => {
 const option = (event) => {
   const category = encodeURIComponent((event.currentTarget.options[event.currentTarget.selectedIndex].value));
   // const url = `http://localhost:3000/services/query?category=${category}`;
-  const url = `http://www.pantherscore.com/services/query?category=${category}`;
+  const url = `https://pantherscores.herokuapp.com/services/query?category=${category}`;
   browseHome.classList.add("d-none")
   request(url);
 }
